450d53d850da9f60d1db86007874551dfe2bcf71
Walterb0b <osca0001@stud.ek.dk>
2025-09-03 10:48:29 +0200

    Commit 1

--- src/TivoliRide.java
+++ src/TivoliRide.java
@@ -1,3 +1,13 @@
 public class TivoliRide {
-    //Hej
+    private double height;
+
+    //Constructor for tivoli ride
+    public tivoliRide(double height){
+        this.height = height;
+    }
+
+    //Method to describe if the person is tall enough
+    public double getDescription(){
+
+    }
 }
